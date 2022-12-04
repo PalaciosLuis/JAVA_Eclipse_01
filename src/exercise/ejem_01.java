a3c19e7339d46652ec19daef9b716e1cc33499d6
Luis Javier <924333523javier@gmail.com>
2022-12-04 16:37:29 -0500

    second commit forEach

--- src/exercise/ejem_01.java
+++ src/exercise/ejem_01.java
@@ -2,6 +2,16 @@ package exercise;
 
 public class ejem_01 {
 	public static void main(String[] args) {
+		//forEach
+		
+		
+		String[]nombres= {"LUIS","JAVIER","PALACIOS","SERNAQUE"};
+		
+		
+		for (String name : nombres) {
+			System.out.println(name+"\n\n");
+			
+		}
 
 	}
 }
